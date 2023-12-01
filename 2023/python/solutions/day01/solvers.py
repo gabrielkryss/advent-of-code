b7bab9b3a1ceedb96ef556b69b0d5f119d7aaa01
gabrielkryss <gabrielkwong971@gmail.com>
2023-12-01 12:51:24 -0800

    clean up python template

---
+++ 2023/python/solutions/day01/solvers.py
@@ -0,0 +1,13 @@
+from aoc.utils import parsing
+
+input_parser = parsing.split_lines
+
+def part1(input_data):
+    return 
+
+
+def part2(input_data):
+    
+    numbers = []
+    
+    return part1(numbers)
