4c7be01d529c341aec097cc3bc960611270c9918
gabrielkryss <gabrielkwong971@gmail.com>
2023-12-01 21:54:04 -0800

    solve for 2023 rust day 1

--- 2023/python/solutions/day01/solvers.py
+++ 2023/python/solutions/day01/solvers.py
@@ -2,12 +2,12 @@ from aoc.utils import parsing
 
 input_parser = parsing.split_lines
 
+
 def part1(input_data):
-    return 
+    
+    raise NotImplementedError()
 
 
 def part2(input_data):
     
-    numbers = []
-    
-    return part1(numbers)
+    raise NotImplementedError()
